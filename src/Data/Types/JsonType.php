b06775962c3dd23dc89639d66d4711bef739f97d
rama <rama@javan.co.id>
2023-09-03 20:45:16 +0700

    fix: type

--- src/Data/Types/JsonType.php
+++ src/Data/Types/JsonType.php
@@ -7,7 +7,7 @@ class JsonType
     /**
      * @throws \JsonException
      */
-    public function __invoke(string $value): array
+    public function __invoke(array $value): array
     {
         return [
             'value' => json_encode($value, JSON_THROW_ON_ERROR),
