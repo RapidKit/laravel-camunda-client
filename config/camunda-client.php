d5ed810b2494516fb65f832b0f43bdc944fc60e2
ramaID <rama@qisthi.dev>
2024-07-22 19:46:25 +0700

    fix: default port 8080

--- config/camunda-client.php
+++ config/camunda-client.php
@@ -2,7 +2,7 @@
 
 // config for RapidKit/LaravelCamundaClient
 return [
-    'url' => env('CAMUNDA_URL', 'http://127.0.0.1:8004/engine-rest'),
+    'url' => env('CAMUNDA_URL', 'http://127.0.0.1:8080/engine-rest'),
     'user' => env('CAMUNDA_USER', 'demo'),
     'password' => env('CAMUNDA_PASSWORD', 'demo'),
     'tenant_id' => env('CAMUNDA_TENANT_ID', ''),
